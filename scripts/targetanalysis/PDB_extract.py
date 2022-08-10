ed6ad37d862b63c5dee69248aa16e9b51c0f55f2
IFilella <isaacfilella@gmail.com>
2022-08-10 18:54:21 +0200

    PDB_analysisTARGET added

--- scripts/targetanalysis/PDB_extract.py
+++ scripts/targetanalysis/PDB_extract.py
@@ -4,16 +4,26 @@ import glob
 import subprocess
 import time
 import argparse
+import shutil
+import errno
+import re
 
 shrodinger_path = '/data/general_software/schrodinger2019-1/'
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description ='Given a directory with .pdb.gz/pdb files, extract all the ligands from them')
-    parser.add_argument('-d', dest="pdbdir", help = "PDBs directory",required=True)
-    args = parser.parse_args()
-
-    pdbdir = args.pdbdir
+def parserfunc():
+    parser = argparse.ArgumentParser(
+        description ='Given a directory with .pdb.gz/pdb files, extract all the ligands from them')
     
+    parser.add_argument('-d', dest="pdbdir", help = "PDBs directory", required=True)
+    parser.add_argument('-o', dest="outdir", help = "Output directory", required=True)
+    
+    args = parser.parse_args()
+    return args
+
+def main():
+    arg = parserfunc()
+    pdbdir = arg.pdbdir
+
     os.chdir(pdbdir)
 
     PDBs = glob.glob("*.pdb*")
@@ -25,7 +35,45 @@ if __name__ == '__main__':
             ID = PDB.replace(".pdb","")
         elif '.pdb' in PDB:
             ID = PDB.replace(".pdb","")
-        print(PDB,ID)
+        print('Extracting %s ligands...' %(ID))
         cmd2 = '%srun split_structure.py -m pdb %s %s.pdb -many_files'%(shrodinger_path,PDB,ID)
-        print(cmd2)
+        #print(cmd2)
         os.system(cmd2)
+
+def organize_output_files():
+    arg = parserfunc()
+    outdir = arg.outdir
+
+    try:
+        os.mkdir(outdir)
+    except OSError:
+        shutil.rmtree(outdir)
+        os.mkdir(outdir)
+    
+    os.chdir(outdir)
+
+    try:
+        os.mkdir("ligand")
+        os.mkdir("receptor")
+        os.mkdir("water")
+        os.mkdir("cof_ion")
+    except OSError:
+        shutil.rmtree("ligand")
+        shutil.rmtree("receptor")
+        shutil.rmtree("water")
+        shutil.rmtree("cof_ion")
+        os.mkdir("ligand")
+        os.mkdir("receptor")
+        os.mkdir("water")
+        os.mkdir("cof_ion")
+    
+    for filename in os.listdir(outdir):
+        file = os.path.isfile(filename)
+        if re.search("ligand", filename):
+            shutil.move("%s/%s"%(outdir, file), "%s/ligand/%s"%(outdir, file))
+
+
+
+
+if __name__ == '__main__':
+    main()
